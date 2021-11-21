47aa459989f5d0405ae8b062d1d8385e46ecc1b8
chrisjwwalker <8091191+chrisjwwalker@users.noreply.github.com>
2021-11-21 17:53:36 +0000

    MC-1: Alphabetise env vars

--- containers.tf
+++ containers.tf
@@ -21,28 +21,28 @@ resource "docker_container" "minecraft" {
   }
 
   env = [
-    "EULA=true",
-    "MOTD=${var.message_of_the_day}",
-    "DIFFICULTY=${var.difficulty}",
-    "WHITELIST=${join(",", var.player_whitelist)}",
-    "OPS=${join(",", var.admins)}",
-    "MAX_PLAYERS=${var.max_player_count}",
-    "MAX_WORLD_SIZE=${var.max_world_size}",
     "ALLOW_NETHER=${var.allow_nether}",
     "ANNOUNCE_PLAYER_ACHIEVEMENTS=${var.announce_player_achievements}",
+    "EULA=true",
+    "DIFFICULTY=${var.difficulty}",
     "FORCE_GAMEMODE=${var.force_gamemode}",
     "GENERATE_STRUCTURES=${var.generate_structures}",
+    "LEVEL_TYPE=${var.level_type}",
     "MAX_BUILD_HEIGHT=${var.max_build_height}",
+    "MAX_PLAYERS=${var.max_player_count}",
+    "MAX_WORLD_SIZE=${var.max_world_size}",
+    "MODE=${var.game_mode}",
+    "MOTD=${var.message_of_the_day}",
+    "OPS=${join(",", var.admins)}",
+    "OVERRIDE_SERVER_PROPERTIES=TRUE",
+    "PVP=${var.pvp}",
+    "SEED=${var.level_seed}",
     "SPAWN_ANIMALS=${var.spawn_animals}",
     "SPAWN_MONSTERS=${var.spawn_monsters}",
     "SPAWN_NPCS=${var.spawn_npcs}",
-    "VIEW_DISTANCE=${var.view_distance}",
-    "SEED=${var.level_seed}",
-    "MODE=${var.game_mode}",
-    "PVP=${var.pvp}",
-    "LEVEL_TYPE=${var.level_type}",
     "TYPE=${var.server_type}",
     "VERSION=${var.minecraft_version}",
-    "OVERRIDE_SERVER_PROPERTIES=TRUE"
+    "VIEW_DISTANCE=${var.view_distance}",
+    "WHITELIST=${join(",", var.player_whitelist)}"
   ]
 }
